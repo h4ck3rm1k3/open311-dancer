c33454c9a9200a6bb323dac1e4befebc0b8ef916
James Michael DuPont <jamesmikedupont@gmail.com>
2012-08-10 08:49:37 +0000

    v2

--- open311/bin/app.pl
+++ open311/bin/app.pl
@@ -5,7 +5,7 @@ set serializer => 'JSON';
 
 
 get qr/.+requests.json.+/ => sub {
-    return {"hello" > "Hello World!"}
+    return {"hello" => "Hello World!"}
 #    "blah"
 #5000/requests.json?legacy=false&extensions=true
     };
